72bbb6247b7b21cbb9a03849cb55f1373bcf7917
Felipe <felipe.moraisoliveira@hotmail.com>
2020-08-05 21:03:14 -0300

    primeiro envio

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int n1, n2;
+
+    cout << "Digite um valor: ";
+    cin >> n1;
+    cout << "Digite um valor: ";
+    cin >> n2;
+
+    cout << "A soma e: " << n1+n2 << endl;
+    cout << "A subtracao e: " << n1-n2 << endl;
+    cout << "A divisao e: " << n1/n2 << endl;
+	cout << "A multiplicacao e: " << n1*n2 << endl;
+
+    return 0;
+}
